bca79d9537752bbba4fdccf58fbaaae30c4549d5
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-09-14 16:00:27 -0300

    Update contacto.jsx

--- src/views/contacto/contacto.jsx
+++ src/views/contacto/contacto.jsx
@@ -1,44 +1,51 @@
+import React from "react";
+import "./contacto.css"; // Asegúrate de especificar la ruta correcta al archivo CSS
+
 const Contacto = () => {
   return (
-    <section class="contact-section">
-      <h2>¡Hola! ¿En qué te podemos ayudar?</h2>
-      <form action="#" method="post">
-        <label class="contact-label" for="nombre">
-          Nombre:
-        </label>
-        <input
-          class="contact-input"
-          type="text"
-          id="nombre"
-          name="nombre"
-          required
-        />
+    <div className="contacto-container">
+      {" "}
+      {/* Agregamos un contenedor */}
+      <section className="contact-section">
+        <h2>¡Hola! ¿En qué te podemos ayudar?</h2>
+        <form action="#" method="post">
+          <label className="contact-label" htmlFor="nombre">
+            Nombre:
+          </label>
+          <input
+            className="contact-input"
+            type="text"
+            id="nombre"
+            name="nombre"
+            required
+          />
 
-        <label class="contact-label" for="email">
-          Correo Electrónico:
-        </label>
-        <input
-          class="contact-input"
-          type="email"
-          id="email"
-          name="email"
-          required
-        />
+          <label className="contact-label" htmlFor="email">
+            Correo Electrónico:
+          </label>
+          <input
+            className="contact-input"
+            type="email"
+            id="email"
+            name="email"
+            required
+          />
 
-        <label class="contact-label" for="comentario">
-          Comentario:
-        </label>
-        <textarea
-          class="contact-textarea"
-          id="comentario"
-          name="comentario"
-          rows="4"
-          required
-        ></textarea>
+          <label className="contact-label" htmlFor="comentario">
+            Comentario:
+          </label>
+          <textarea
+            className="contact-textarea"
+            id="comentario"
+            name="comentario"
+            rows="4"
+            required
+          ></textarea>
 
-        <input class="contact-submit" type="submit" value="Enviar" />
-      </form>
-    </section>
+          <input className="contact-submit" type="submit" value="Enviar" />
+        </form>
+      </section>
+    </div>
   );
 };
 
