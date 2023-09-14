a1d73fadf573f8a8936d094656b031d8cbf9872d
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-09-14 12:04:36 -0300

    navBar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,7 +1,22 @@
+import "./navBar.css";
+import conectando from "../../media/conectando.png";
 const NavBar = () => {
   return (
-    <div>
-      <h1>navBar</h1>
+    <div class="header-fixed">
+      <img src={conectando} alt="Texto Alternativo" class="logo" />
+      <div class="header-links">
+        <a href="#eventos">Eventos</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#contacto">Contacto</a>
+      </div>
+      <div class="login-links">
+        <a href="#inicio-sesion" class="login-link">
+          Iniciar Sesión
+        </a>
+        <a href="#registro" class="login-link">
+          Registrarse
+        </a>
+      </div>
     </div>
   );
 };
