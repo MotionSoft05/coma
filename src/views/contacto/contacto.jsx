35dc6e35fc3845c9788d9506bb6e4d80bd058a80
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-09-14 17:09:04 -0300

    Update contacto.jsx

--- src/views/contacto/contacto.jsx
+++ src/views/contacto/contacto.jsx
@@ -1,49 +1,55 @@
 import React from "react";
 import "./contacto.css"; // Asegúrate de especificar la ruta correcta al archivo CSS
+import leo from "../../media/leo.jpg";
 
 const Contacto = () => {
   return (
-    <div className="contacto-container">
-      {" "}
-      {/* Agregamos un contenedor */}
+    <div>
       <section className="contact-section">
-        <h2>¡Hola! ¿En qué te podemos ayudar?</h2>
-        <form action="#" method="post">
-          <label className="contact-label" htmlFor="nombre">
-            Nombre:
-          </label>
-          <input
-            className="contact-input"
-            type="text"
-            id="nombre"
-            name="nombre"
-            required
-          />
+        <div className="form-container">
+          <div className="image-container">
+            <img className="contact-image" src={leo} alt="Imagen de ejemplo" />
+          </div>
+          <div className="form-content">
+            <h2>¡Hola! ¿En qué te podemos ayudar?</h2>
+            <form action="#" method="post">
+              <label className="contact-label" htmlFor="nombre">
+                Nombre:
+              </label>
+              <input
+                className="contact-input"
+                type="text"
+                id="nombre"
+                name="nombre"
+                required
+              />
 
-          <label className="contact-label" htmlFor="email">
-            Correo Electrónico:
-          </label>
-          <input
-            className="contact-input"
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
+              <label className="contact-label" htmlFor="email">
+                Correo Electrónico:
+              </label>
+              <input
+                className="contact-input"
+                type="email"
+                id="email"
+                name="email"
+                required
+              />
 
-          <label className="contact-label" htmlFor="comentario">
-            Comentario:
-          </label>
-          <textarea
-            className="contact-textarea"
-            id="comentario"
-            name="comentario"
-            rows="4"
-            required
-          ></textarea>
+              <label className="contact-label" htmlFor="comentario">
+                Comentario:
+              </label>
+              <textarea
+                className="contact-textarea"
+                id="comentario"
+                name="comentario"
+                rows="4"
+                required
+              ></textarea>
 
-          <input className="contact-submit" type="submit" value="Enviar" />
-        </form>
+              <input className="contact-submit" type="submit" value="Enviar" />
+            </form>
+          </div>
+        </div>
       </section>
     </div>
   );
